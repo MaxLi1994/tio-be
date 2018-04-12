3ee400f97177f3e38ab29eed6ed4b9ebbc9a4c16
JieyingXu <31418649+JieyingXu@users.noreply.github.com>
2018-04-12 15:22:06 -0400

    changed api doc

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -3,6 +3,7 @@ package controllers;
 import Models.User;
 import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
+import com.jfinal.ext.interceptor.GET;
 import com.jfinal.ext.interceptor.POST;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
@@ -89,6 +90,7 @@ public class UserController extends Controller {
      * @apiError {Msg} 2 User input is empty string or whitespaces.
      * @apiError {Msg} 3 User input doesn't correspond to any database record.
      */
+    @Before(GET.class)
     public void login() {
         String account = getPara("account");
         String password = getPara("password");
@@ -113,25 +115,32 @@ public class UserController extends Controller {
     }
 
     /**
-     * @api {post} /user/changeNickName Change the user's nickname given his/her id.
-     * @apiName changeNickName
+     * @api {post} /user/changeNickname Change the user's nickname given his/her id.
+     * @apiName changeNickname
      * @apiGroup user
+     *
      * @apiParam {String} user_id Current user's id.
-     * @apiParam {String} newNickName User input new nickname.
-     * @apiSuccess {Json} UserObject Updated user model.
-     * @apiError {Json} IncompleteInput User didn't complete all fields.
-     * @apiError {Json} IllegalEmptyInput User input is empty string or whitespaces.
-     * @apiError {Json} IdFormatError User_id must be an integer.
-     * @apiError {Json} UserNotFound Provided user id is not found in the database.
+     * @apiParam {String} newNickname User input new nickname.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     *     "account": string,
+     *     "nickname": string,
+     *     "id": int
+     * }
+     * @apiError {Json} 1 User didn't complete all fields.
+     * @apiError {Json} 2 User input is empty string or whitespaces.
+     * @apiError {Json} 3 User_id must be an integer.
+     * @apiError {Json} 4 Provided user id is not found in the database.
      */
-    public void changeNickName() {
+    @Before(POST.class)
+    public void changeNickname() {
         String user_idStr = getPara("user_id");
-//        System.out.println(user_idStr);
-        String newNickName = getPara("newNickName");
-        if (user_idStr == null || newNickName == null) {
+        String newNickname = getPara("newNickname");
+        if (user_idStr == null || newNickname == null) {
             setAttr("code", -1);
             setAttr("msg", "Lack input.");
-        } else if (user_idStr.trim().equals("") || newNickName.trim().equals("")) {
+        } else if (user_idStr.trim().equals("") || newNickname.trim().equals("")) {
             setAttr("code", -1);
             setAttr("msg", "User input cannot be empty string or pure whitespaces.");
         } else {
@@ -153,7 +162,8 @@ public class UserController extends Controller {
                     setAttr("code", -1);
                     setAttr("msg", "User not found.");
                 } else {
-                    myUser.set("nickname", newNickName).update();
+                    myUser.set("nickname", newNickname).update();
+                    myUser.remove("password");
                     setAttr("code", 0);
                     setAttr("data", myUser);
                 }
@@ -163,29 +173,36 @@ public class UserController extends Controller {
     }
 
     /**
-     * @api {post} /user/changePassWord Change the user's password given his/her id.
-     * @apiName changePassWord
+     * @api {post} /user/changePassword Change the user's password given his/her id.
+     * @apiName changePassword
      * @apiGroup user
+     *
      * @apiParam {String} user_id Current user's id.
-     * @apiParam {String} oldPassWord User input old password.
-     * @apiParam {String} newPassWord User input new password.
-     * @apiSuccess {Json} SuccessMsg User's password field has been changed in database.
-     * @apiError {Json} IncompleteInput User didn't complete all fields.
-     * @apiError {Json} IllegalEmptyInput User input is empty string or whitespaces.
-     * @apiError {Json} UserNotFound Provided user id is not found in the database.
-     * @apiError {Json} IdFormatError User_id must be an integer.
-     * @apiError {Json} UserNotFound Provided user id is not found in the database.
-     * @apiError {Json} IncompatibleOldPassword User input old password is not compatible with database record.
+     * @apiParam {String} oldPassword User input old password.
+     * @apiParam {String} newPassword User input new password.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     *     "msg": string
+     * }
+     *
+     * @apiError {Json} 1 User didn't complete all fields.
+     * @apiError {Json} 2 User input is empty string or whitespaces.
+     * @apiError {Json} 3 Provided user id is not found in the database.
+     * @apiError {Json} 4 User_id must be an integer.
+     * @apiError {Json} 5 Provided user id is not found in the database.
+     * @apiError {Json} 6 User input old password is not compatible with database record.
      */
-    public void changePassWord() {
+    @Before(POST.class)
+    public void changePassword() {
         String user_idStr = getPara("user_id");
-        String oldPassWord = getPara("oldPassWord");
-        String newPassWord = getPara("newPassWord");
+        String oldPassword = getPara("oldPassword");
+        String newPassword = getPara("newPassword");
 
-        if (user_idStr == null || oldPassWord == null || newPassWord == null) {
+        if (user_idStr == null || oldPassword == null || newPassword == null) {
             setAttr("code", -1);
             setAttr("msg", "Lack input");
-        } else if (user_idStr.trim().equals("") || oldPassWord.trim().equals("") || newPassWord.trim().equals("")) {
+        } else if (user_idStr.trim().equals("") || oldPassword.trim().equals("") || newPassword.trim().equals("")) {
             setAttr("code", -1);
             setAttr("msg", "User input cannot be empty string or pure whitespaces.");
         } else {
@@ -205,12 +222,12 @@ public class UserController extends Controller {
                     setAttr("code", -1);
                     setAttr("msg", "User not found");
                 } else {
-                    String userOldPassWord = myUser.getStr("password");
-                    if (!userOldPassWord.equals(oldPassWord)) {
+                    String userOldPassword = myUser.getStr("password");
+                    if (!userOldPassword.equals(oldPassword)) {
                         setAttr("code", -1);
                         setAttr("msg", "Password not compatible.");
                     } else {
-                        myUser.set("password", newPassWord).update();
+                        myUser.set("password", newPassword).update();
                         setAttr("code", 0);
                         setAttr("msg", "Password successfully reset.");
                     }
